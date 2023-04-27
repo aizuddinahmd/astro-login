bf0aaea7fb91ea719076658dacf5896fbc0ccbd6
Judin <aizuddinahmd0408@gmail.com>
2023-04-27 20:28:42 +0800

    Added Content

--- src/components/content.svelte
+++ src/components/content.svelte
@@ -1,7 +1,7 @@
 <script>
     import supabase from "../utils/supabase";
     import {onMount} from "svelte";
-  import Logout from "./logout.svelte";
+  import Logout from "./logOutBtn.svelte";
 
   let userLoggedIn = false;
 
@@ -9,7 +9,7 @@
     const user = await supabase.auth.getUser();
     console.log(user);
 
-    if (user.data.user == null) { // Check if running in the browser
+    if (user.data.user == null) { 
       window.location.href = "/";
     } else {
         userLoggedIn = true
@@ -22,7 +22,7 @@
 </script>
 
 {#if userLoggedIn}
-  <h1>Content</h1>
+  <h1>This is a protected content</h1>
   <Logout />
 {/if}
 
